01b244541c2b4b2c425f8e72f2c5ac7263b955d8
App <shobhitsagar109@gmail.com>
2017-12-26 23:51:34 +0530

    Added two toasts

--- app/src/main/java/com/shobhitsagar/toast/MainActivity.java
+++ app/src/main/java/com/shobhitsagar/toast/MainActivity.java
@@ -1,8 +1,8 @@
 package com.shobhitsagar.toast;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
@@ -20,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater inflater = getLayoutInflater();
 
         View CustomToastView = inflater.inflate(R.layout.toast_layout, null);
+        Toast customToast = new Toast(context);
+
+        customToast.setView(CustomToastView);
+        customToast.setDuration(Toast.LENGTH_LONG);
+        customToast.show();
+    }
+
+    public void rectToastBtn(View view) {
+        Context context = getApplicationContext();
+        LayoutInflater inflater = getLayoutInflater();
+
+        View CustomToastView = inflater.inflate(R.layout.toast_layout_rect, null);
         Toast customToast = new Toast(context);
 
         customToast.setView(CustomToastView);
